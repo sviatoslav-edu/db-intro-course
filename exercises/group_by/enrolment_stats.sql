be5a1972a38405e39ed4e4e220572b333c7f7acf
Sviatoslav Opatsa <opatsa.s.i.-io45@edu.kpi.ua>
2026-05-14 17:59:42 +0000

    Зробив завдання 6: enrolment_stats.sql

--- exercises/group_by/enrolment_stats.sql
+++ exercises/group_by/enrolment_stats.sql
@@ -9,3 +9,12 @@
 --          - роком навчання (зростання)
 
 -- Рішення:
+SELECT 
+    e.start_year AS student_year,                                    -- рік навчання (student_year)
+    COUNT(DISTINCT e.course_id) AS number_of_courses,                -- кількість унікальних курсів у цьому році
+    COUNT(e.student_id) AS number_of_enrolments,                     -- загальна кількість записів (усі рядки)
+    COUNT(CASE WHEN e.grade IS NOT NULL THEN 1 END) AS number_of_students_with_grade -- кількість студентівб з оцінками
+FROM enrolment e
+GROUP BY e.start_year -- групуємо за роком навчання
+ORDER BY 
+    student_year ASC;
