35e3686540d7726a6c7d4db7876a59e39da9899b
Sviatoslav Opatsa <opatsa.s.i.-io45@edu.kpi.ua>
2026-05-14 17:35:23 +0000

    Зробив завдання 4: course_prerequisites.sql

--- exercises/joins/course_prerequisites.sql
+++ exercises/joins/course_prerequisites.sql
@@ -8,3 +8,13 @@
 --          - назвою курсу, потім за назвою пре-реквізиту
 
 -- Рішення:
+
+SELECT 
+    c.name AS course_name,
+    p.name AS prerequisite_name
+FROM course c
+LEFT JOIN course_prerequisite cp ON c.course_id = cp.course_id
+LEFT JOIN course p ON cp.prerequisite_course_id = p.course_id
+ORDER BY 
+    course_name ASC, 
+    prerequisite_name ASC;
