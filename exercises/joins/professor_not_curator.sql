71f20ab3101226eb205612283672a6b2402975ce
Sviatoslav Opatsa <opatsa.s.i.-io45@edu.kpi.ua>
2026-05-14 17:32:13 +0000

    Виконав завдання 3: professor_not_curator.sql

--- exercises/joins/professor_not_curator.sql
+++ exercises/joins/professor_not_curator.sql
@@ -5,5 +5,22 @@
 --          - посада (job)
 --      Результат відсортувати за:
 --          - повним іменем викладача
-
 -- Рішення:
+
+-- Очікувані колонки результату:
+--          - повне ім'я викладача (professor_name)
+--          - посада (job)
+SELECT 
+    CONCAT(p.first_name, ' ', p.last_name) AS professor_name, -- повне ім'я викладача (professor_name)
+    pr.job                                                    -- посада (job)
+FROM professor pr
+JOIN person p ON pr.person_id = p.person_id -- особисті дані викладача
+WHERE pr.status = 'викладає' -- Викладачів зі статусом 'викладає'
+  AND NOT EXISTS (
+      SELECT 1 
+      FROM student_group sg 
+      WHERE sg.curator_id = pr.professor_id -- перевіряємо, чи є він куратором хоч якоїсь групи
+  ) -- які не є куратором жодної студентської групи
+-- Результат відсортувати за: повним іменем викладача
+ORDER BY 
+    professor_name ASC;
