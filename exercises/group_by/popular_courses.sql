28fa8dd7cc8a78a7d4ab1e2abb866f195bc4c1f4
Sviatoslav Opatsa <opatsa.s.i.-io45@edu.kpi.ua>
2026-05-14 06:12:41 +0000

    Зробив завдання 7: popular_courses.sql

--- exercises/group_by/popular_courses.sql
+++ exercises/group_by/popular_courses.sql
@@ -8,3 +8,15 @@
 --          - кількістю студентів (спадання), потім за назвою курсу
 
 -- Рішення:
+
+SELECT 
+    c.name AS course_name,                                       -- назва курсу (course_name)
+    COUNT(e.student_id) AS student_count,                        -- кількість студентів (student_count)
+    ROUND(AVG(e.grade), 2) AS avg_grade                          -- середній бал, округлений до 2 знаків (avg_grade)
+FROM course c
+JOIN enrolment e ON c.course_id = e.course_id -- підключаємо записи на курс
+GROUP BY c.course_id, c.name -- групуємо за ID та назвою курсу
+HAVING COUNT(e.student_id) > 100 -- знайти курси, на які записано більше ніж 100 студентів
+ORDER BY 
+    student_count DESC,  -- Сортування з умови
+    course_name ASC;
